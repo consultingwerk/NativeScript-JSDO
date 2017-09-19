a329f14db83e059c64508ffffe5de4b35a1f9c21
Radu Nicoara <radu.nicoara@acorn.ro>
2017-09-19 12:59:15 +0300

    implement browser storage emulator that uses the nativescript application settings API; add Edsel's jQuery wrapper to remove jQuery dependency, remove unnecessary files

---
+++ index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var progress_jsdo_1 = require("./lib/progress.jsdo");
+exports.progress = progress_jsdo_1.progress;
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyJpbmRleC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOztBQUFBLHFEQUErQztBQUF0QyxtQ0FBQSxRQUFRLENBQUEiLCJzb3VyY2VzQ29udGVudCI6WyJleHBvcnQgeyBwcm9ncmVzcyB9IGZyb20gXCIuL2xpYi9wcm9ncmVzcy5qc2RvXCI7Il19
